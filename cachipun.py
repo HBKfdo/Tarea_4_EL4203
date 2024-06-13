a97e06382ad9b39d9fe318f903de6fd1deb4dbf6
Iñaki <Inaki.escobar@ug.uchile.cl>
2024-06-13 14:09:42 -0400

    agrego la función para hacer sumas

---
+++ cachipun.py
@@ -0,0 +1,16 @@
+def piedra_papel_tijera():
+    import random
+    opciones = ["piedra", "papel", "tijera"]
+    decision = input("Elije piedra, papel o tijera: ")
+    computador = random.choice(opciones)
+    print(f"La computadora eligió: {computador}")
+    if decision == computador:
+        return "Es un empate"
+    elif (decision == "piedra" and computador == "tijeras") or \
+         (decision == "tijeras" and computador == "papel") or \
+         (decision == "papel" and computador == "piedra"):
+        return "¡Ganaste!"
+    else:
+        return "Perdiste :("
+resultado = piedra_papel_tijera()
+print(resultado)
